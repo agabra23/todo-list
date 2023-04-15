5df71db533540d6418bc79e93984d6142e9a4506
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-04-15 15:34:09 -0400

    Add list renaming feature

--- src/displayController.js
+++ src/displayController.js
@@ -94,18 +94,14 @@ export const displayController = (() => {
       const listWrapper = document.createElement("div");
       listWrapper.dataset.index = 0;
       const listTitle = document.createElement("h4");
-      const gearDiv = document.createElement("div");
-      gearDiv.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="width: 25px" fill="currentColor" viewBox="0 0 24 24"><title>cog-outline</title><path d="M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8M12,10A2,2 0 0,0 10,12A2,2 0 0,0 12,14A2,2 0 0,0 14,12A2,2 0 0,0 12,10M10,22C9.75,22 9.54,21.82 9.5,21.58L9.13,18.93C8.5,18.68 7.96,18.34 7.44,17.94L4.95,18.95C4.73,19.03 4.46,18.95 4.34,18.73L2.34,15.27C2.21,15.05 2.27,14.78 2.46,14.63L4.57,12.97L4.5,12L4.57,11L2.46,9.37C2.27,9.22 2.21,8.95 2.34,8.73L4.34,5.27C4.46,5.05 4.73,4.96 4.95,5.05L7.44,6.05C7.96,5.66 8.5,5.32 9.13,5.07L9.5,2.42C9.54,2.18 9.75,2 10,2H14C14.25,2 14.46,2.18 14.5,2.42L14.87,5.07C15.5,5.32 16.04,5.66 16.56,6.05L19.05,5.05C19.27,4.96 19.54,5.05 19.66,5.27L21.66,8.73C21.79,8.95 21.73,9.22 21.54,9.37L19.43,11L19.5,12L19.43,13L21.54,14.63C21.73,14.78 21.79,15.05 21.66,15.27L19.66,18.73C19.54,18.95 19.27,19.04 19.05,18.95L16.56,17.95C16.04,18.34 15.5,18.68 14.87,18.93L14.5,21.58C14.46,21.82 14.25,22 14,22H10M11.25,4L10.88,6.61C9.68,6.86 8.62,7.5 7.85,8.39L5.44,7.35L4.69,8.65L6.8,10.2C6.4,11.37 6.4,12.64 6.8,13.8L4.68,15.36L5.43,16.66L7.86,15.62C8.63,16.5 9.68,17.14 10.87,17.38L11.24,20H12.76L13.13,17.39C14.32,17.14 15.37,16.5 16.14,15.62L18.57,16.66L19.32,15.36L17.2,13.81C17.6,12.64 17.6,11.37 17.2,10.2L19.31,8.65L18.56,7.35L16.15,8.39C15.38,7.5 14.32,6.86 13.12,6.62L12.75,4H11.25Z" /></svg>`;
       listWrapper.classList.add("list-item");
       listWrapper.classList.add("all-tasks-list");
       listWrapper.dataset.index = 0;
       if (listArr[0].active) listWrapper.classList.add("active");
       listTitle.classList.add("list-item-title");
       listTitle.setAttribute.id = `listItemTitle0`;
-      gearDiv.classList.add("nav-gear-div");
 
       listWrapper.appendChild(listTitle);
-      listWrapper.appendChild(gearDiv);
       container.appendChild(listWrapper);
 
       listTitle.textContent = "All Tasks";
@@ -114,26 +110,68 @@ export const displayController = (() => {
         const listWrapper = document.createElement("div");
         listWrapper.dataset.index = i + 1;
         const listTitle = document.createElement("h4");
-        const gearDiv = document.createElement("div");
-        gearDiv.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="width: 25px" fill="currentColor" viewBox="0 0 24 24"><title>cog-outline</title><path d="M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8M12,10A2,2 0 0,0 10,12A2,2 0 0,0 12,14A2,2 0 0,0 14,12A2,2 0 0,0 12,10M10,22C9.75,22 9.54,21.82 9.5,21.58L9.13,18.93C8.5,18.68 7.96,18.34 7.44,17.94L4.95,18.95C4.73,19.03 4.46,18.95 4.34,18.73L2.34,15.27C2.21,15.05 2.27,14.78 2.46,14.63L4.57,12.97L4.5,12L4.57,11L2.46,9.37C2.27,9.22 2.21,8.95 2.34,8.73L4.34,5.27C4.46,5.05 4.73,4.96 4.95,5.05L7.44,6.05C7.96,5.66 8.5,5.32 9.13,5.07L9.5,2.42C9.54,2.18 9.75,2 10,2H14C14.25,2 14.46,2.18 14.5,2.42L14.87,5.07C15.5,5.32 16.04,5.66 16.56,6.05L19.05,5.05C19.27,4.96 19.54,5.05 19.66,5.27L21.66,8.73C21.79,8.95 21.73,9.22 21.54,9.37L19.43,11L19.5,12L19.43,13L21.54,14.63C21.73,14.78 21.79,15.05 21.66,15.27L19.66,18.73C19.54,18.95 19.27,19.04 19.05,18.95L16.56,17.95C16.04,18.34 15.5,18.68 14.87,18.93L14.5,21.58C14.46,21.82 14.25,22 14,22H10M11.25,4L10.88,6.61C9.68,6.86 8.62,7.5 7.85,8.39L5.44,7.35L4.69,8.65L6.8,10.2C6.4,11.37 6.4,12.64 6.8,13.8L4.68,15.36L5.43,16.66L7.86,15.62C8.63,16.5 9.68,17.14 10.87,17.38L11.24,20H12.76L13.13,17.39C14.32,17.14 15.37,16.5 16.14,15.62L18.57,16.66L19.32,15.36L17.2,13.81C17.6,12.64 17.6,11.37 17.2,10.2L19.31,8.65L18.56,7.35L16.15,8.39C15.38,7.5 14.32,6.86 13.12,6.62L12.75,4H11.25Z" /></svg>`;
+        const pencilDiv = document.createElement("div");
+        const trashDiv = document.createElement("div");
+        pencilDiv.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" style="width: 25px" viewBox="0 0 24 24"><title>pencil-outline</title><path d="M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z" /></svg>`;
+        trashDiv.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="width: 25px" viewBox="0 0 24 24"><title>trash-can-outline</title><path fill="currentColor" d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>`;
         listWrapper.classList.add("list-item");
         if (list.active) listWrapper.classList.add("active");
         listTitle.classList.add("list-item-title");
         listTitle.setAttribute.id = `listItemTitle${i + 1}`;
-        gearDiv.classList.add("nav-gear-div");
+        pencilDiv.classList.add("nav-pencil-div");
+        trashDiv.classList.add("nav-trash-div");
 
         listWrapper.appendChild(listTitle);
-        listWrapper.appendChild(gearDiv);
+        listWrapper.appendChild(pencilDiv);
+        listWrapper.appendChild(trashDiv);
         container.appendChild(listWrapper);
 
         listTitle.textContent = list.title;
       });
 
-      const gears = document.querySelectorAll(".nav-gear-div");
-      gears.forEach((gear) => {
-        gear.addEventListener("click", (e) => {
+      const pencils = document.querySelectorAll(".nav-pencil-div");
+      pencils.forEach((pencil) => {
+        pencil.addEventListener("click", (e) => {
           e.stopPropagation();
-          console.log(`Clicked this gear: ${gear}`);
+          console.log(`Edit this list`);
+
+          const currentTitle = pencil.previousElementSibling;
+          currentTitle.setAttribute("contenteditable", true);
+          currentTitle.addEventListener("focus", () => {
+            const range = document.createRange();
+            const selection = window.getSelection();
+            range.setStart(currentTitle, currentTitle.childNodes.length);
+            range.setEnd(currentTitle, currentTitle.childNodes.length);
+            selection.removeAllRanges();
+            selection.addRange(range);
+          });
+          currentTitle.focus();
+          currentTitle.addEventListener("blur", () => {
+            currentTitle.setAttribute("contenteditable", false);
+            const headerTitle = document.getElementById("listTitle");
+            if (currentTitle != null) {
+              if (currentTitle.parentNode.classList.contains("active"))
+                headerTitle.textContent = currentTitle.textContent;
+
+              listArr[currentTitle.parentElement.dataset.index].title =
+                currentTitle.textContent;
+            }
+          });
+
+          currentTitle.addEventListener("keydown", (e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              currentTitle.blur();
+            }
+          });
+        });
+      });
+
+      const trashCans = document.querySelectorAll(".nav-trash-div");
+      trashCans.forEach((trashCan) => {
+        trashCan.addEventListener("click", (e) => {
+          console.log(`Delete this list`);
+          e.stopPropagation();
         });
       });
     });
